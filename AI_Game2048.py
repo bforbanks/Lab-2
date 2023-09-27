b5f4d51c361073163ba40519fb5ff977dd2d2531
xxbenja16 <benja020201@live.dk>
2023-09-27 23:35:41 +0200

    -1 for illigal

--- AI_Game2048.py
+++ AI_Game2048.py
@@ -55,7 +55,7 @@ class SimGame2048:
 
 
         if(not sim.move_is_legal(self.first_step)):
-            return [0]
+            return [-1]
         
         # loop for running n simulations
         for i in range(self.simulation_count):
